3cf4797ef614d66b64b1b4a3bfc5bfda406c09ed
Ayelegun Kayode Michael <billmike1994@gmail.com>
2019-01-20 22:20:28 +0100

    redesign 404 page

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,8 +3,17 @@ import React, { Component } from 'react'
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        Hello page not found
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1>404</h1>
+            <h1>error</h1>
+            <h2>Page not found</h2>
+            <h3>The requested URL<span className="text-danger">
+            {this.props.location.pathname}
+            </span> was not found</h3>
+          </div>
+        </div>
       </div>
     )
   }
